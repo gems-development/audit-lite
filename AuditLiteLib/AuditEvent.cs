a4235f38efa8900b2472e03e93ae1a2f725f3688
Petr Nikitin <nip2001@mail.ru>
2025-03-20 15:43:36 +0600

    + Сущность события + Сбор окружения сущности

---
+++ AuditLiteLib/AuditEvent.cs
@@ -0,0 +1,16 @@
+﻿namespace AuditLiteLib;
+
+public class AuditEvent
+{
+    private string EventType { get; set; }
+    private EventEnvironment EventEnvironment { get; set; }
+    private DateTime EventDate { get; set; }
+    private Dictionary<string, object>? OptionalFields { get; set; } 
+    public AuditEvent(string eventType, EventEnvironment eventEnvironment, Dictionary<string, object>? optionalFields = null)
+    {
+        EventType = eventType;
+        EventEnvironment = eventEnvironment;
+        OptionalFields = optionalFields;
+        EventDate = DateTime.Now;
+    }
+}
