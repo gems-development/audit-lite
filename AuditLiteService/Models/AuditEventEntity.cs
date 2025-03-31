b6d0ac6494795132dbb93760448479c59c040e0f
Julia Kolistratova <Kolistratova.Julija@mail.ru>
2025-03-31 17:22:28 +0600

    + Создан проект AuditLiteService + Добавлены модели для хранения события аудита в БД + Создан файл .proto для контракта с клиентом + Написан сервис AuditLoggerService

---
+++ AuditLiteService/Models/AuditEventEntity.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuditLiteService.Models;
+
+public class AuditEventEntity
+{
+    [Key]
+    public int Id { get; set; }  // Первичный ключ
+
+    [Required]
+    public string EventType { get; set; } = null!;
+
+    [Required]
+    public DateTime EventDate { get; set; }
+    
+    public int EventEnvironmentId { get; set; }
+    public EventEnvironmentEntity EventEnvironmentEntity { get; set; } = null!;
+    
+    public List<CustomFieldEntity> CustomFields { get; set; } = new();
+}
---
+++ AuditLiteService/Models/EventEnvironmentEntity.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuditLiteService.Models;
+
+public class EventEnvironmentEntity
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string UserName { get; set; } = null!;
+
+    [Required]
+    public string MethodName { get; set; } = null!;
+
+    [Required]
+    public string MachineName { get; set; } = null!;
+
+    [Required]
+    public string IpAddress { get; set; } = null!;
+}
