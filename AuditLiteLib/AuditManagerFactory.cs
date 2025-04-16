d794df7b6343164f19f0f685d3bd74daf41e9af8
Petr Nikitin <nip2001@mail.ru>
2025-04-16 23:47:11 +0600

    + Настроил DI для ключевых компонентов (AuditConfig, EventBuffer, AuditClient, AuditManager) + Создал метод расширения для регистрации зависимостей в DI-контейнере: + Реализовал создание экземпляра AuditManager через фабричный метод. + Модернизировал AuditManager под DI * Переписал ConsoleApp * Рефакторинг

---
+++ AuditLiteLib/AuditManagerFactory.cs
@@ -0,0 +1,16 @@
+﻿using AuditLiteLib.Configuration;
+using AuditLiteLib.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuditLiteLib
+{
+	public static class AuditManagerFactory
+	{
+		public static AuditManager Create(Action<AuditConfigBuilder> configure)
+		{
+			var services = new ServiceCollection();
+			services.AddAuditLite(configure);
+			return services.BuildServiceProvider().GetRequiredService<AuditManager>();
+		}
+	}
+}
---
+++ AuditLiteLib/Extensions/AuditServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using AuditLiteLib.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace AuditLiteLib.Extensions
+{
+	public static class AuditServiceCollectionExtensions
+	{
+		public static IServiceCollection AddAuditLite(this IServiceCollection services, Action<AuditConfigBuilder> configure)
+		{
+			// Создаем конфигурацию через переданный делегат
+			var configBuilder = new AuditConfigBuilder();
+			configure(configBuilder);
+			var auditConfig = configBuilder.Build();
+
+			services.AddSingleton(auditConfig); 
+			services.AddSingleton<EventBuffer>(provider => new EventBuffer(auditConfig.MaxBufferSize));
+			services.AddSingleton<AuditClient>(provider => new AuditClient(auditConfig.ServerUrl));
+			services.AddLogging(builder => builder.AddConsole());
+
+			services.AddSingleton<AuditManager>();
+
+			return services;
+		}
+	}
+}
